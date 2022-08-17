a4e9a7cf45047548441c5eb2399372c788ceeb3f
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-08-17 15:43:35 +0300

    Display table with tracks

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+class Tracks {
+    constructor(number, artist, song) {
+        this.number = number;
+        this.artist = artist;
+        this.song = song
+    }
+}
+
+const tracksTable = document.getElementById('playlist')
+const playList = [
+    {author: "LED ZEPPELIN",song:"STAIRWAY TO HEAVEN"},
+    {author: "QUEEN", song:"BOHEMIAN RHAPSODY"},
+    {author: "LYNYRD SKYNYRD", song:"FREE BIRD"},
+    {author: "DEEP PURPLE", song:"SMOKE ON THE WATER"},
+    {author: "JIMI HENDRIX", song:"ALL ALONG THE WATCHTOWER"},
+    {author: "AC/DC", song:"BACK IN BLACK"},
+    {author: "QUEEN", song:"WE WILL ROCK YOU"},
+    {author: "METALLICA", song:"ENTER SANDMAN"}
+    ];
+
+playList.forEach(displayPlaylist);
+
+function displayPlaylist(track) {
+    tracksTable.innerHTML += `
+        <tr>
+            <td class="track__date">
+                <p>1977</p>
+            </td>
+            <td class="track__author">
+                <p>${track.author}</p>
+            </td>
+            <td class="track__name">
+                <p>${track.song}</p>
+            </td>
+            <td class="track__option">
+                <button class="btn-small" id="${track.author}">Open</button>
+            </td>
+        </tr>
+    `
+}
+
